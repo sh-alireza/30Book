2c846570b063b869dceaf9995b7abab314d537ca
alireza <a.sharifi45@yahoo.com>
2021-04-13 23:01:35 +0430

    Update scripts.js
    grid & sliders finished... next is carousel

--- scripts.js
+++ scripts.js
@@ -1,7 +1,43 @@
-$(document).ready(function (){
-    $('#nav-btn').click(function (){
-        $('#nav-btn').toggleClass('fa-bars',100)
-        $('#nav-btn').toggleClass('fa-times',100)
-        $('.menu').toggleClass('active',500)
-    })
-})
+// menu btn
+function menuBtn () {
+    var elem = document.getElementById('nav-btn');
+    elem.classList.toggle("fa-bars");
+    elem.classList.toggle("fa-times");
+}
+// grid sliders
+var index = 1;
+show(index,0);
+show(index,1);
+show(index,2);
+function show(n,a) { // "a" is a parameter to let the function know which element to use
+    var i;
+    var slides = document.getElementsByClassName("slides"+a);
+    var tabs = document.getElementsByClassName("tab");
+    if (n > slides.length) {
+        index = 1
+    }
+    if (n < 1) {
+        index = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    if (a===0){
+        for (i = 0; i < tabs.length; i++) {
+            tabs[i].className = tabs[i].className.replace(" tab-active", "");
+        }
+        tabs[index-1].className += " tab-active";
+    }
+    slides[index-1].style.display = " block";
+}
+function plusSlides(n,a) {
+    show(index += n,a);
+}
+function currentSlide(n) {
+    show(index = n,0);
+}
+function infinity(){
+    plusSlides(1,1);
+    plusSlides(1,2);
+}
+setInterval(infinity, 5000);
